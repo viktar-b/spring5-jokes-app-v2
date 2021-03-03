a0d7333bcd0cd5ab04882fa3da27ea1d3aa984a1
viktar-b <viktar.beliakou@gmail.com>
2021-03-03 17:09:48 +0000

    config added

--- src/main/java/guru/springframework/spring5jokesappv2/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/spring5jokesappv2/config/ChuckConfiguration.java
@@ -3,8 +3,10 @@ package guru.springframework.spring5jokesappv2.config;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 /*
- *
+ * Without that configuration, JokeServiceImpl constructor would need to instantiate
+ * ChuckNorrisQuotes() class
  */
 @Configuration
 public class ChuckConfiguration {
